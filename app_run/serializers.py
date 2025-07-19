b81077719864e501186df23a5ead51a1dea4fbbd
Petrak.M <petrak.maly@keemail.me>
2025-07-19 22:09:42 +0300

    fix: format date_time field

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -80,6 +80,7 @@ class ChallengeSerializer(serializers.ModelSerializer):
 
 
 class PositionSerializer(serializers.ModelSerializer):
+    date_time = serializers.DateTimeField(format='%Y-%m-%dT%H:%M:%S.%f')
     class Meta:
         model = Position
         fields = [
